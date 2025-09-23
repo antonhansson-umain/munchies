40e14de5d600efdc8046a0c1a783a50b92a947f2
Anton Hansson <antonxhansson@gmail.com>
2025-09-23 13:37:01 +0200

    feat: logo

---
+++ components/Logo.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/cn";
+import Image from "next/image";
+
+type LogoProps = React.HTMLAttributes<HTMLImageElement>;
+
+export default function Logo({ className }: LogoProps) {
+  return (
+    <Image
+      src={"/logo.svg"}
+      alt="Munchies Logo"
+      width={273.42}
+      height={40}
+      className={className}
+    />
+  );
+}
