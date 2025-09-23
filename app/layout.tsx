ecbdc0dc8852a831fb786945dbff5eb54a647a27
Anton Hansson <antonxhansson@gmail.com>
2025-09-23 11:10:50 +0200

    style: basic layout set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import type { Metadata } from "next";
 import "./globals.css";
+import Logo from "@/components/Logo";
 
 const SFPro = localFont({
   src: "../fonts/SF-Pro.ttf",
@@ -18,7 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${SFPro.className} antialiased`}>{children}</body>
+      <body
+        className={`${SFPro.className} antialiased max-w-screen-xl mx-auto pl-10 grid grid-rows-[auto_1fr] grid-cols-[239px_1fr] min-h-screen gap-x-5`}
+      >
+        <header className="col-span-2 h-36 flex items-center pt-1">
+          <Logo />
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
