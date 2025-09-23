ffdbe06a0c4b46011bb19140330bf022d872e687
Anton Hansson <antonxhansson@gmail.com>
2025-09-23 10:17:48 +0200

    style: remove old bg color assignment

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${SFPro.className} antialiased !bg-[#FAFAFA]`}>
-        {children}
-      </body>
+      <body className={`${SFPro.className} antialiased`}>{children}</body>
     </html>
   );
 }
