149cfe9bf17c2c76043f4cf4e6b732ba5b910ade
Anton Hansson <anton.hansson@umain.com>
2025-09-25 10:41:20 +0200

    style: filters active state

--- components/FilterChip.tsx
+++ components/FilterChip.tsx
@@ -18,9 +18,10 @@ export default function FilterChip({
     <button
       onClick={handleClick}
       className={cn(
-        "px-3 py-2 border-[0.6px] border-black/10 rounded-lg flex gap-2 w-max transition-colors hover:bg-background",
+        "px-3 py-2 border-[0.6px] border-black/10 rounded-lg flex gap-2 w-max transition-colors hover:bg-background hover:!border-primary-green",
         {
-          "!bg-primary-green/50": values.includes(filter.value),
+          "!bg-primary-green hover:!bg-primary-green/80 text-white":
+            values.includes(filter.value),
         },
         className
       )}
