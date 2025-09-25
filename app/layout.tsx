6c5d54381d671a704bd71d6364d4c111153e5241
Anton Hansson <anton.hansson@umain.com>
2025-09-25 09:18:29 +0200

    feat: go to / when clicking logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import type { Metadata } from "next";
 import "./globals.css";
 import Logo from "@/components/Logo";
+import Link from "next/link";
 
 const SFPro = localFont({
   src: [
@@ -34,7 +35,9 @@ export default function RootLayout({
         className={`${SFPro.className} antialiased max-w-[1440px] grid grid-rows-[auto_1fr] grid-cols-[239px_1fr] min-h-screen gap-x-5 mx-auto pl-10`}
       >
         <header className="col-span-2 h-36 flex items-center pt-1">
-          <Logo />
+          <Link href={"/"} aria-label="Munchies">
+            <Logo />
+          </Link>
         </header>
         {children}
       </body>
