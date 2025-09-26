a5b4a87cd4b165cf67cd11675f4cc2187e7bd960
Anton Hansson <anton.hansson@umain.com>
2025-09-26 11:50:43 +0200

    refactor: make skeletons syncronous funcs

--- components/Restaurants/loading.tsx
+++ components/Restaurants/loading.tsx
@@ -1,7 +1,7 @@
 import H1 from "../Headings/H1";
 import RestaurantCardSkeleton from "../RestaurantCard/loading";
 
-export default async function RestaurantsSkeleton() {
+export default function RestaurantsSkeleton() {
   return (
     <div className="flex flex-col gap-5 sm:gap-8 sm:pr-10 max-sm:px-6 sm:pl-5">
       <H1 className="max-sm:text-[20px]">Restaurants</H1>
