7af5444f1c88fe8d2cdab4997f7c9f8d45c2f6c0
Anton Hansson <anton.hansson@umain.com>
2025-09-26 11:18:17 +0200

    refactor: filter cards skeleton

---
+++ components/FilterCards/index.tsx
@@ -0,0 +1,28 @@
+import { FiltersResponse } from "@/types/FilterResponse";
+import FilterCard from "../FilterCard";
+
+export default async function FilterCards({
+  categories,
+}: {
+  categories: FiltersResponse["filters"];
+}) {
+  return (
+    <div
+      className={`flex items-center gap-2.5 max-w-0 min-w-full sm:min-w-[calc(100%+12px)] pr-10 max-sm:px-6 pb-6 sm:pb-10 sm:pl-8 sm:-ml-3 ${
+        categories ? "overflow-x-scroll" : "overflow-hidden"
+      }`}
+    >
+      {categories.map((f) => (
+        <FilterCard
+          key={f.id}
+          filter={{
+            label: f.name,
+            value: f.id,
+            groupKey: "category",
+            image: f.image_url,
+          }}
+        />
+      ))}
+    </div>
+  );
+}
